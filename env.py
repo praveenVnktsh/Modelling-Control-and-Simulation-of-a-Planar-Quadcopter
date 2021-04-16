2552032ac241ddb98862c39b3f7fd6b85dacf4e8
Varun Jain <varun.jain@iitgn.ac.in>
2021-04-17 01:57:56 +0530

    distance corrected

--- env.py
+++ env.py
@@ -32,7 +32,7 @@ class quadEnv(gym.Env):
         x = np.random.rand(1)*(self.xlims[1]-self.xlims[0]) + self.xlims[0]
         y = np.random.rand(1)*(self.ylims[1])
 
-        self.goal = np.array([x, y])
+        self.goal = np.array([x, y]).squeeze()
         self.distance = np.linalg.norm(self.goal-self.current)
         self.initdistance = self.distance
 
@@ -44,6 +44,7 @@ class quadEnv(gym.Env):
         self.currentstep = 0
 
         if self.render:
+            plt.close()
             plt.ion()
             self.xval = [position[0], ]
             self.yval = [position[1], ]
@@ -73,14 +74,16 @@ class quadEnv(gym.Env):
     def getobs(self, state):
         x, y = state[0], state[2]
         self.current = np.array([x, y])
-        distance = np.linalg.norm(self.goal-np.array([x, y]))
+        distance = np.linalg.norm(self.goal-self.current)
+        self.distance = distance
         return distance
 
     def cal_reward(self):
         reward = -self.distance/self.initdistance
 
     def is_done(self):
-        return self.distance <= self.sse or self.currentstep >= self.timesteps or self.current[1] < 0
+        inbounds = self.xlims[0] <= self.current[0] <= self.xlims[1] and self.ylims[0] <= self.current[1] <= self.ylims[1]
+        return self.distance <= self.sse or self.currentstep >= self.timesteps or not inbounds
 
     def render_screen(self):
         theta = self.quad.state[7]
@@ -93,9 +96,10 @@ class quadEnv(gym.Env):
             [-self.quad.L*np.cos(theta) + x, self.quad.L*np.cos(theta) + x])
         self.arm_plt.set_ydata([y + self.quad.L *
                                 np.sin(theta), y - self.quad.L*np.sin(theta)])
+        self.ax.set_title(f"Error:{self.distance:2f}")
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-        plt.pause(0.001)
+        # plt.pause(0.001)
 
     def step(self, action):
         Thrust = action[0]
@@ -124,5 +128,9 @@ class quadEnv(gym.Env):
 if __name__ == '__main__':
     env = quadEnv(render=True)
     print("Current Error:", env.reset())
-    for i in range(100):
-        env.step([10, 0.001])
+    while(True):
+        env.reset()
+        for i in range(100):
+            _, _, done, _ = env.step([10, 0.001])
+            if done:
+                break
